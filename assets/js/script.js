509dac433f6dde06882ea1118bd804b33678a810
Toor <92349350+B-Toor@users.noreply.github.com>
2022-02-02 07:59:46 -0800

    added modal code in javascript (broken)

--- assets/js/script.js
+++ assets/js/script.js
@@ -215,3 +215,25 @@ searchButton.on("click", function (event) {
     //add fetchMovie but exceeded api calls so blocked me out
 });
 //need to call two functions here fetch drink and fetch movie
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("searchButton");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    if (displayDrink !== null) {
+        modal.style.display = "block";
+    }
+};
+
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
